7b92c4a0c95fd93aee058c00710e6698a8aba1c7
hk210 <hk21051999@gmail.com>
2021-06-02 16:23:46 +0530

    SpaceXCrewMembers-Assignment

---
+++ app/src/main/java/com/hk210/spacexcrew/DeatilsViewActivity.java
@@ -0,0 +1,14 @@
+package com.hk210.spacexcrew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeatilsViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deatils_view);
+    }
+}
